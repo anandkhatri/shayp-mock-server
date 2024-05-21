95b7924a21484f74628b4a02d31dc7b58be3f436
anand <anand.khatriau@gmail.com>
2024-05-21 16:00:19 +1000

    init commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const apimock = require('@ng-apimock/core');
+const devInterface = require('@ng-apimock/dev-interface');
+const express = require('express');
+const app = express();
+app.set('port', 9999);
+
+apimock.processor.process({
+    src: 'mocks',
+    patterns: { // optional
+        mocks: '**/*Mock.json', // optional: default is '**/*.mock.json'		
+        presets: '**/*preset.json',
+    },
+    watch: true,
+});
+
+function logRequests(req, res, next) {
+    console.log('Headers:', req.pathname);
+    // console.log('Headers:', req.headers);
+    next();
+}
+
+app.use(logRequests);
+app.use(apimock.middleware);
+app.use('/dev-interface', express.static(devInterface));
+
+
+app.listen(app.get('port'), () => {
+    console.log('@ng-apimock/core running on port', app.get('port'));
+    console.log('@ng-apimock/dev-interface is available under /dev-interface');
+});
